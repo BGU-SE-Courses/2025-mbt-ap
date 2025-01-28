2b0778f5877d3725f71a6cf7fbc21a66c151bd18
hadasprintz <hadaspr100@gmail.com>
2025-01-28 19:43:58 +0200

    fix paths

--- Provengo/Moodle/data/data.js
+++ Provengo/Moodle/data/data.js
@@ -7,11 +7,11 @@ const paths = {
   },
   coursePage: {
     myCoursesLink: "//*[contains(text(),'My courses') and @role='menuitem']",
-    courseLink: "//div[1]/a[1]/span[3]/span[2][contains(text(),'${e.courseName}')]",
+    courseLink: "//div[1]/div[1]/a[1]/span[3]/span[2]",
   },
   forumPage: {
-    forumLink: "//a[contains(.,'${forumName}')]",
-    commentLink: "//div[1]/table[1]/tbody[1]/tr[1]/th[1]/div[1]/div[1]/a[contains(text(),'${e.commentName}')]",
+    forumLink: "//div[1]/ul[1]/li[3]/div[1]/div[2]/ul[1]/li[1]/div[1]/div[2]/div[2]/div[1]/div[1]/a[1]",
+    commentLink: "//a[contains(@title, 'why there is no coffee anymore?!')]",
     deleteButton: "//div[1]/div[2]/div[2]/div[1]/a[3]",
     confirmDeleteButton: "//div[2]/form[1]/button[1]",
     addCommentButton: "//div[1]/div[1]/div[2]/a[1]",
